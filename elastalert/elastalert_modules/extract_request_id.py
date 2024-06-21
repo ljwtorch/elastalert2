00cb3a988ab65131d8c2d6db50078dedd2498634
lijuwei <ju_wei.li@ceasia.cn>
2024-06-21 16:52:21 +0800

    add enhance

--- elastalert/elastalert_modules/extract_request_id.py
+++ elastalert/elastalert_modules/extract_request_id.py
@@ -3,14 +3,9 @@ import re
 
 
 class Extract_Requestid(BaseEnhancement):
-
-    # The enhancement is run against every match
-    # The match is passed to the process function where it can be modified in any way
-    # ElastAlert will do this for each enhancement linked to a rule
     def process(self, match):
         # 定义正则表达式模式
         pattern = r"\b[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}\b"
-
         # 检查 'message' 字段是否存在于match中
         if 'message' in match:
             log_message = match['message']
